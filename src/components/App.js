19f71b5ccbe1597098aafcec197b2739bb28b893
Shelvy <shelvy511@gmail.com>
2022-04-27 21:14:28 -0500

    update.

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import axios from "axios"
-require("dotenv").config()
+import { useEffect } from "react"
+import { Buffer } from "buffer"
+//require("dotenv").config()
 
 const App = () => {
 
     const client_id = 'CLIENT_ID'; // Your client id
-    const client_secret = 'CLIENT_SECRET'; // Your secret
-    
+    const client_secret = 'CLIENT_SECRET'; // Your secret 
     // your application requests authorization
+   useEffect(() => { 
     const authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
       },
       form: {
         grant_type: 'client_credentials'
       },
       json: true
     };
-    
+   
     axios.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
     
         // use the access token to access the Spotify Web API
         const token = body.access_token;
         const options = {
-          url: 'https://api.spotify.com/v1/users/jmperezperez',
+          url: '/search',
           headers: {
             'Authorization': 'Bearer ' + token
           },
@@ -36,6 +38,7 @@ const App = () => {
         });
       }
     });
+   }, [])  
 
 
   return (
